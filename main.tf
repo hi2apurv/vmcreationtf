b829aa620c014b60732a146c821d9e387e6120d8
hi2apurv <71092286+hi2apurv@users.noreply.github.com>
2021-03-15 12:00:48 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,200 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+  }
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name = "Master"
+  location = "eastus"
+
+    tags = {
+        environment = "Terraform"
+    }
+	}
+
+# Your Terraform code goes here...
+
+resource "azurerm_virtual_network" "MasterVnet" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.rg.name
+
+    tags = {
+        environment = "Terraform"
+    }
+}
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.MasterVnet.name
+    address_prefixes       = ["10.0.2.0/24"]
+}
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "eastus"
+    resource_group_name          = azurerm_resource_group.rg.name
+    allocation_method            = "Static"
+
+    tags = {
+        environment = "Terraform"
+    }
+}
+resource "azurerm_network_security_group" "Masternsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+security_rule {
+        name                       = "HTTPS"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+security_rule {
+        name                       = "HTTP"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+security_rule {
+        name                       = "RDP"
+        priority                   = 1004
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3389"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+security_rule {
+        name                       = "PORT8080"
+        priority                   = 1005
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "8080"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform"
+    }
+}
+resource "azurerm_network_interface" "Masternic" {
+    name                        = "myNIC"
+    location                    = "eastus"
+    resource_group_name         = azurerm_resource_group.rg.name
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+    }
+
+    tags = {
+        environment = "Terraform"
+    }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id      = azurerm_network_interface.Masternic.id
+    network_security_group_id = azurerm_network_security_group.Masternsg.id
+}
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = azurerm_resource_group.rg.name
+    }
+
+    byte_length = 8
+}
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = azurerm_resource_group.rg.name
+    location                    = "eastus"
+    account_replication_type    = "LRS"
+    account_tier                = "Standard"
+
+    tags = {
+        environment = "Terraform"
+    }
+}
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
+
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+    name                  = "MasterVM"
+    location              = "eastus"
+    resource_group_name   = azurerm_resource_group.rg.name
+    network_interface_ids = [azurerm_network_interface.Masternic.id]
+    size                  = "Standard_DS1_v2"
+
+    os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "Mastervm"
+    admin_username = "azureuser"
+	admin_password = "Master@1234"
+    disable_password_authentication = false
+
+    admin_ssh_key {
+        username       = "azureuser"
+        public_key     = tls_private_key.example_ssh.public_key_openssh
+    }
+
+    boot_diagnostics {
+        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
